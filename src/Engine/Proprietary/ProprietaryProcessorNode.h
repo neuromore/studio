7c10abcc74cf2f60df56923f2d69f26404572c9b
Andrew Grosser <dioptre@gmail.com>
2019-11-15 07:37:47 -0800

    added missing files

---
+++ src/Engine/Proprietary/ProprietaryProcessorNode.h
@@ -0,0 +1,40 @@
+/****************************************************************************
+**
+** Copyright 2019 neuromore co
+** Contact: https://neuromore.com/contact
+**
+** Commercial License Usage
+** Licensees holding valid commercial neuromore licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and neuromore. For licensing terms
+** and conditions see https://neuromore.com/licensing. For further
+** information use the contact form at https://neuromore.com/contact.
+**
+** neuromore Public License Usage
+** Alternatively, this file may be used under the terms of the neuromore
+** Public License version 1 as published by neuromore co with exceptions as 
+** appearing in the file neuromore-class-exception.md included in the 
+** packaging of this file. Please review the following information to 
+** ensure the neuromore Public License requirements will be met: 
+** https://neuromore.com/npl
+**
+****************************************************************************/
+#ifndef __NEUROMORE_PROPRIETARYPROCESSORNODE_H
+#define __NEUROMORE_PROPRIETARYPROCESSORNODE_H
+
+#include "../Graph/ProcessorNode.h"
+
+namespace Proprietary {
+
+// Base Class for Proprietary Processor Nodes. Consult our license for more info about this class.
+class ENGINE_API ProprietaryProcessorNode : public ProcessorNode
+{
+public:
+   inline ProprietaryProcessorNode(Graph* parent, ChannelProcessor* processor) : ProcessorNode(parent, processor) { }
+   inline ~ProprietaryProcessorNode() { }
+};
+
+};
+
+#endif
