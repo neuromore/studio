6219138cfeca2ac4aa6502a1ea3211299604be1c
Clint Banzhaf <clint.banzhaf@zds.de>
2020-04-30 14:35:27 +0200

    qt multimedia plugins windows vs

--- deps/include/qt/QtMultimedia/private/qtmultimedia-config_p.h
+++ deps/include/qt/QtMultimedia/private/qtmultimedia-config_p.h
@@ -1,6 +1,5 @@
 #define QT_FEATURE_alsa -1
 #define QT_FEATURE_directshow 1
-#define QT_FEATURE_wmf_player -1
 #define QT_FEATURE_directshow_player 1
 #define QT_FEATURE_evr 1
 #define QT_FEATURE_gpu_vivante -1
@@ -16,5 +15,6 @@
 #define QT_FEATURE_resourcepolicy -1
 #define QT_FEATURE_wasapi 1
 #define QT_FEATURE_wmf 1
+#define QT_FEATURE_wmf_player 1
 #define QT_FEATURE_wmsdk 1
 #define QT_FEATURE_wshellitem 1
