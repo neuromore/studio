11d4789265d30ed904258a9f6e3aab44c27c755f
Clint Banzhaf <clint.banzhaf@zds.de>
2022-09-23 10:57:39 +0200

    update

--- deps/build/make/platforms/dist.mk
+++ deps/build/make/platforms/dist.mk
@@ -141,6 +141,7 @@ dist: dist-prep dist-x64 dist-arm64
 	@cp $(DISTDIR)/$(NAME).Info.plist $(DISTDIR)/$(NAME)/$(NAME).app/Contents/Info.plist
 	@cp $(DISTDIR)/$(NAME).provisionprofile $(DISTDIR)/$(NAME)/$(NAME).app/Contents/embedded.provisionprofile
 	@sed -i'.orig' -e 's/{VERSION}/${VERSION3}/g' $(DISTDIR)/$(NAME)/$(NAME).app/Contents/Info.plist
+	@sed -i'.orig' -e 's/{OSXSDKVER}/${OSXSDKVER}/g' $(DISTDIR)/$(NAME)/$(NAME).app/Contents/Info.plist
 	@rm $(DISTDIR)/$(NAME)/$(NAME).app/Contents/Info.plist.orig
 ifeq ($(APPLE_DIST_STORE),true)
 	@echo [SIG] $(NAME).app
