ff58a866fedfd3bfc2c66c54f465808def5df409
Clint Banzhaf <clint@banzhaf.pro>
2022-10-13 14:11:42 +0200

    fix linux

--- src/Engine/neuromoreEngine.h
+++ src/Engine/neuromoreEngine.h
@@ -34,7 +34,17 @@
 #define NEUROMORE_EXPORT
 #endif
 
+#ifndef BOOL
 #define BOOL int
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
 
 #ifdef __cplusplus
 namespace neuromoreEngine
