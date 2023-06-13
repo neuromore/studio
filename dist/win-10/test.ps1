ca80fec0e675d6250799d0bd69e4965ec4f22a1a
Clint Banzhaf <clint@banzhaf.pro>
2023-06-13 14:43:53 +0200

    update

--- dist/win-10/test.ps1
+++ dist/win-10/test.ps1
@@ -1,17 +1,14 @@
-﻿$basefolder  = "."
-$filespath   = ".\Studio\x64"
-$filesfilter = "*"
-$xmlloadpath = "Studio.wxs"
-$xmlsavepath = (Resolve-Path $xmlloadpath).Path + ".mod"
-
-#Set-Location $basefolder
+﻿param (
+  [Parameter(Mandatory)] [System.String] $in,
+  [Parameter(Mandatory)] [System.String] $out,
+  [Parameter(Mandatory)] [System.String] $path
+)
 
 # Load WXS
-$xml = [xml](Get-Content -Path $xmlloadpath)
+$xml = [xml](Get-Content -Path $in)
 $featurenode = $xml.Wix.Package.Feature
 
-$node = $xml.Wix.Package.StandardDirectory | where {$_.Id -eq 'ProgramFiles6432Folder'}
-$node = $node.Directory | where {$_.Id -eq 'INSTALLFOLDER'}
+###############################################################################################
 
 function process
 {
@@ -45,13 +42,17 @@ function process
     else
     {     
       $newfile = $xml.CreateElement("File", "http://wixtoolset.org/schemas/v4/wxs")
-      $newfile.SetAttribute("Source", (Resolve-Path -Relative $c.FullName))   
+#      $newfile.SetAttribute("Source", (Resolve-Path -Relative $c.FullName))
+      $newfile.SetAttribute("Source", $c.FullName)
+
       $ext = [System.IO.Path]::GetExtension($c)
       if ($ext -eq ".exe") {
          if ($rootfolder) {
            $newfile.SetAttribute("Id", $plainname)
          }
-         $newrule = $xml.CreateElement("fire:FirewallException", "http://wixtoolset.org/schemas/v4/wxs/firewall")             
+         $newrule = $xml.CreateElement(
+           "fire:FirewallException", 
+           "http://wixtoolset.org/schemas/v4/wxs/firewall")             
          $newrule.SetAttribute("Name", $plainname)        
          $newrule.SetAttribute("Profile", "private")
          $newrule.SetAttribute("Scope", "any")
@@ -69,6 +70,13 @@ function process
   }
 }
 
-process $filespath $node 1
+###############################################################################################
 
-$xml.save($xmlsavepath)
+$node = $xml.Wix.Package.StandardDirectory | where {$_.Id -eq 'ProgramFiles6432Folder'}
+$node = $node.Directory | where {$_.Id -eq 'INSTALLFOLDER'}
+
+# Modify WXS
+process $path $node 1
+
+# Save WXS
+$xml.save($out)
